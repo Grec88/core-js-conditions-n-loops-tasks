ca783bf7ca96be9f938860580245bd51a550ff7e
grec88 <Lihtinmann88@yandex.ru>
2025-01-12 14:32:05 +0300

    feat: add solution to is Palindrome task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -252,8 +252,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let revertedStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    revertedStr += str[i];
+  }
+  return revertedStr === str;
 }
 
 /**

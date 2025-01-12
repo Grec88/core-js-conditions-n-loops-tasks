dd9433c9f2e2ef210e8bd61d138ba1384351c7a7
grec88 <Lihtinmann88@yandex.ru>
2025-01-12 18:39:59 +0300

    feat: add solution of shuffleChar without meeting time criteria

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -518,8 +518,25 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let resStr = str;
+
+  for (let iter = 0; iter < iterations; iter += 1) {
+    let oddChars = '';
+    let evenChars = '';
+
+    for (let i = 0; i < resStr.length; i += 1) {
+      if (i % 2 === 0) {
+        evenChars += resStr[i];
+      } else {
+        oddChars += resStr[i];
+      }
+    }
+
+    resStr = evenChars + oddChars;
+  }
+
+  return resStr;
 }
 
 /**
